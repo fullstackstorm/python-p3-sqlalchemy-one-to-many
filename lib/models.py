0fcbb24193f05bdcf4c0b855054ded2ffa3823f7
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-08-14 02:32:51 -0700

    Remove the deprecation warning.

--- lib/models.py
+++ lib/models.py
@@ -1,13 +1,15 @@
 from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref, declarative_base
+#from sqlalchemy.ext.declarative import declarative_base
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 }
 metadata = MetaData(naming_convention=convention)
 
-Base = declarative_base(metadata=metadata)
+#Base = declarative_base(metadata=metadata)
+Base = declarative_base(metadata=metadata, class_registry=dict())
+
 
 class Game(Base):
     __tablename__ = 'games'
